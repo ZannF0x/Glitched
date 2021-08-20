41d46ca2fbfe8ad7826f2873784e5ee190de2935
Zannardy Andrade Oliveira <78097970+ZannF0x@users.noreply.github.com>
2021-08-20 11:59:16 -0300

    Impliementação - FormCadastro

--- app/src/main/java/com/zannardyapps/glitched/forms/FormRegister.kt
+++ app/src/main/java/com/zannardyapps/glitched/forms/FormRegister.kt
@@ -1,43 +1,31 @@
 package com.zannardyapps.glitched.forms
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
-import com.zannardyapps.glitched.databinding.ActivityFormLoginBinding
+import com.zannardyapps.glitched.databinding.ActivityFormRegisterBinding
 
-class FormLogin : AppCompatActivity() {
-    private lateinit var binding: ActivityFormLoginBinding
+class FormRegister : AppCompatActivity() {
+    private lateinit var binding: ActivityFormRegisterBinding
 
     private lateinit var editTextEmail: EditText
     private lateinit var editTextSenha: EditText
     private lateinit var buttonEntrar: Button
-    private lateinit var textViewCadastre: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityFormLoginBinding.inflate(layoutInflater)
+        binding = ActivityFormRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
         supportActionBar!!.hide()
 
         initComponentsLayout()
 
-        textViewCadastre.setOnClickListener {
-            openFormRegister()
-        }
     }
 
     private fun initComponentsLayout(){
         editTextEmail = binding.editTextEmail
         editTextSenha = binding.editTextSenha
         buttonEntrar = binding.buttonEntrar
-        textViewCadastre = binding.textCadastrese
-    }
-
-    private fun openFormRegister(){
-        val intent = Intent(this, FormRegister::class.java)
-        startActivity(intent)
     }
 }
