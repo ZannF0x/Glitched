f9e58feaa4f351ac2533c44c277d56c16125cc1f
Zannardy Andrade Oliveira <78097970+ZannF0x@users.noreply.github.com>
2021-08-20 23:14:08 -0300

    Implementação - Exibir erro no EditText da FormCadastro

--- app/src/main/java/com/zannardyapps/glitched/forms/FormRegister.kt
+++ app/src/main/java/com/zannardyapps/glitched/forms/FormRegister.kt
@@ -21,6 +21,27 @@ class FormRegister : AppCompatActivity() {
 
         initComponentsLayout()
 
+        buttonEntrar.setOnClickListener {
+
+            when {
+
+                editTextEmail.text.isEmpty() -> {
+                    editTextEmail.error = "Erro! Preencha o campo de E-mail."
+                    if (editTextEmail.text.isNotEmpty()) editTextEmail.error = null
+                }
+
+                editTextSenha.text.isEmpty() -> {
+                    editTextSenha.error = "Erro! Preencha o campo de Senha."
+                    if (editTextSenha.text.isNotEmpty()) editTextSenha.error = null
+                }
+
+                else -> {
+                    // TODO: 20/08/2021 Implementer FirebaseAuth
+                }
+            }
+
+        }
+
     }
 
     private fun initComponentsLayout(){
