caeab9a68cc8455a234c1297f1f0d356bc1743de
Zannardy Andrade Oliveira <78097970+ZannF0x@users.noreply.github.com>
2021-08-29 15:21:38 -0300

    Implementação - Menu na StoreScrenn / Opção de deslogar

--- app/src/main/java/com/zannardyapps/glitched/screenstore/StoreScreen.kt
+++ app/src/main/java/com/zannardyapps/glitched/screenstore/StoreScreen.kt
@@ -3,6 +3,10 @@ package com.zannardyapps.glitched.screenstore
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.viewModels
 import com.zannardyapps.glitched.R
 import com.zannardyapps.glitched.forms.FormLogin
@@ -16,9 +20,32 @@ class StoreScreen : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_store_screen)
 
-
     }
 
+    // menu methods
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        //return super.onCreateOptionsMenu(menu)
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_storescreen, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        //return super.onOptionsItemSelected(item)
+        return when(item.itemId){
+            R.id.configSettings -> {
+                Toast.makeText(this, "Configurações...", Toast.LENGTH_LONG).show()
+                true
+            }
+            R.id.configExit -> {
+                returnFormLogin()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    // method to return FormLogin
     private fun returnFormLogin(){
         viewModel.logout()
         val intent = Intent(this, FormLogin::class.java)
